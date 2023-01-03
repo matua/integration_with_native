55fd2e8ac29cd0e3f7dd7799c80227a1fc78dda3
Georgii <matuageorge@gmail.com>
2023-01-02 21:00:49 -0500

    setup continued

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -5,6 +5,44 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    TextEditingController textEditingController = TextEditingController();
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 100,
+          left: 10,
+          right: 10,
+          bottom: 100,
+        ),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                style: const TextStyle(fontSize: 50),
+                decoration:
+                    const InputDecoration(border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black))),
+                controller: textEditingController,
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: Container(
+                    height: 90,
+                    width: 90,
+                    decoration: BoxDecoration(color: Colors.green, border: Border.all()),
+                    child: const Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Press Me!',
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
